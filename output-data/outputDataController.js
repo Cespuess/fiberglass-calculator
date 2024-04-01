9a4fb634e731882fdb411156c1dff7f4db57b734
Carles Espuñes <cespuess@gmail.com>
2024-04-01 18:12:11 +0200

    renderData modificado, que solo muestre los datos de la consulta actual.

--- output-data/outputDataController.js
+++ output-data/outputDataController.js
@@ -1,7 +1,5 @@
 import { renderData } from "./outputDataView.js";
 
 export function outputDataController(outputData, data) {
-  const answer = document.createElement('div');
-  answer.innerHTML = renderData(data);
-  outputData.appendChild(answer);
+  outputData.innerHTML = renderData(data);
 }
